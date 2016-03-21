06afd49b80dc39060b0e2b4cd629857d214a127c
Petr Viktorin <encukou@gmail.com>
2016-03-21 14:29:23 +0100

    Add a convenience script to combine 2 A5 PDFs to one A4

---
+++ combine-a4.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Combine two A5 PDFs into one A4
+
+if [ "$1" == "" ]; then
+    echo Usage: $0 filename
+
+    echo Example: $0 basic-functions/basic-functions-cs.pdf
+else
+    pdfjam --landscape --paper a4paper --nup 2x1 "$1" 1,1 -o combined-a4.pdf
+fi
