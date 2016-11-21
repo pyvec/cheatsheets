018721fa0d398df0d5d903ce2cb4068da85b662b
Petr Viktorin <encukou@gmail.com>
2016-11-21 16:11:55 +0100

    Speed up conversion by using Inkscape's batch mode

--- export-all.py
+++ export-all.py
@@ -3,8 +3,15 @@
 import glob
 import re
 import subprocess
+import shlex
 
+lines = []
 for filename in glob.glob('*/*.svg'):
-    print(filename)
     output = re.sub(r'\.svg$', '.pdf', filename)
-    subprocess.check_call(['inkscape', filename, '-A', output])
+    lines.append([filename, '-A', output])
+
+proc = subprocess.Popen(['inkscape', '--shell'], stdin=subprocess.PIPE)
+stdin_text = '\n'.join(' '.join(shlex.quote(w) for w in s) for s in lines) + '\n'
+print(stdin_text)
+proc.communicate(stdin_text.encode('utf-8'))
+print()
