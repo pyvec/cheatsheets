4a30c80907dbebe6eda4290b89a0a26b902dee39
Petr Viktorin <encukou@gmail.com>
2021-04-12 19:57:30 +0200

    Adjust export-all script for Inkscape 1.0

--- export-all.py
+++ export-all.py
@@ -4,14 +4,31 @@ import glob
 import re
 import subprocess
 import shlex
+import asyncio
+import os
 
-lines = []
-for filename in glob.glob('*/*.svg'):
+async def export_one(sem, filename):
     output = re.sub(r'\.svg$', '.pdf', filename)
-    lines.append([filename, '-A', output])
+    cmd = [
+        'inkscape',
+        filename,
+        '--export-area=page',
+        '--batch-process',
+        '--export-type=pdf',
+        f'--export-filename={output}'
+    ]
+    async with sem:
+        print(f'Converting {filename}\n', end='')
+        proc = await asyncio.create_subprocess_exec(*cmd)
+        await proc.communicate()
+        if proc.returncode != 0:
+            raise RuntimeError(f'Error converting {filename}\n', end='')
+        print(f'Done converting {filename}\n', end='')
 
-proc = subprocess.Popen(['inkscape', '--shell'], stdin=subprocess.PIPE)
-stdin_text = '\n'.join(' '.join(shlex.quote(w) for w in s) for s in lines) + '\n'
-print(stdin_text)
-proc.communicate(stdin_text.encode('utf-8'))
-print()
+
+async def export_all():
+    sem = asyncio.Semaphore(len(os.sched_getaffinity(0)))
+    tasks = (export_one(sem, name) for name in glob.glob('*/*.svg'))
+    return await asyncio.gather(*tasks)
+
+asyncio.run(export_all())
